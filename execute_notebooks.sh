e928d9078f5cd45c5d7e0cfe2bdcadf21632291d
Guillaume Le Treut <guillaume.letreut@czbiohub.org>
2021-12-19 11:13:11 -0800

    Added outputs of Jupyter notebooks.

--- execute_notebooks.sh
+++ execute_notebooks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script execute all notebooks. It must be run from within the docker container.
 
-for fname in 1-clustering 2-fluxes 3-import_cssegi 4-distances 5-SIR_dynamics_fit
+for fname in 1-clustering 2-fluxes 3-import_cssegi 4-distances 5-SIR_dynamics_fit 6-simulations/61-simplified_model 6-simulations/62-distance_cutoff 7-wave_analysis/71-ode_gamma_eq0 7-wave_analysis/72-ode_gamma_neq0
 do
   echo Executing notebook ${fname}.ipynb
   jupyter nbconvert --to notebook --inplace --execute notebooks/${fname}.ipynb
