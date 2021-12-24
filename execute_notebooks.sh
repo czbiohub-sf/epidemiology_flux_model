8057df66123572abf07497e8201e82cd2f51eb8a
Guillaume Le Treut <guillaume.letreut@czbiohub.org>
2021-12-24 04:18:05 -0800

    2d lattice simulations.
    Added the notebook in which the aspect ratio is varied.

--- execute_notebooks.sh
+++ execute_notebooks.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # This script execute all notebooks. It must be run from within the docker container.
 
-for fname in 1-clustering 2-fluxes 3-import_cssegi 4-distances 5-SIR_dynamics_fit 6-simulations/61-simplified_model 6-simulations/62-distance_cutoff 7-wave_analysis/71-ode_gamma_eq0 7-wave_analysis/72-ode_gamma_neq0
+for fname in 1-clustering \
+             2-fluxes \
+             3-import_cssegi \
+             4-distances \
+             5-SIR_dynamics_fit \
+             6-simulations/61-simplified_model \
+             6-simulations/62-distance_cutoff \
+             6-simulations/63-lattice_2d_nneighbors_loop_beta \
+             6-simulations/64-lattice_2d_nneighbors_loop_n2 \
+             7-wave_analysis/71-ode_gamma_eq0 \
+             7-wave_analysis/72-ode_gamma_neq0 \
+             8-infectivity_matrix_properties
 do
   echo Executing notebook ${fname}.ipynb
   jupyter nbconvert --to notebook --inplace --execute notebooks/${fname}.ipynb
